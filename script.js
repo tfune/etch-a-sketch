53955d80317b77c6a1ebad7c8cd5d98319c36a4c
tfune <trevor.fune@gmail.com>
2023-02-16 23:47:36 -0800

    Create button that changes number of squares per side

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 const container = document.querySelector('.container');
+let sideLength = 16
 
-for (i = 0; i < 256; i++) {
+function createGrid () {
+    for (i = 0; i < (sideLength * sideLength); i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
+    };
 };
+createGrid();
 
-const squares = document.querySelectorAll('.square')
+let squares = document.querySelectorAll('.square');
 
 squares.forEach(square => {
     square.addEventListener('mouseover', function (e) {
         square.classList.add('hover');
     })
 })
+
+const button = document.querySelector('.button');
+
+button.addEventListener('click', function (e) {
+    sideLength = prompt("How many squares per side? (max 100)", 16);
+    resetGrid(container);
+    container.style.setProperty('--container-columns', sideLength);
+    container.style.setProperty('--container-rows', sideLength);
+    createGrid();
+    squares = document.querySelectorAll('.square')
+    squares.forEach(square => {
+        square.addEventListener('mouseover', function (e) {
+            square.classList.add('hover');
+        })
+    })
+})
+
+function resetGrid (container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
